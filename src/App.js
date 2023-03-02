dda8b82a0a2d3053211185f3c595dcaa64471d56
tahminaroshni <tahminatanni12@gmail.com>
2023-03-02 22:52:51 +0600

    login page added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
 import Projects from "./pages/Projects";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <>
+      <Navbar />
       <div className="app bg-slate-900 text-slate-50 min-h-screen container mx-auto py-5">
-        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
       <Footer />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,14 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="h-14 border-b border-sky-700 px-10">
-      <Link to="/" className="text-sky-400 text-xl">Proxima</Link>
+    <div className="h-20 bg-slate-900 text-slate-50 border-b border-sky-700 px-10 flex justify-between items-center">
+      <h2>
+        <Link to="/" className="text-sky-400 text-xl">Proxima</Link>
+      </h2>
+      <nav className="flex gap-5">
+        <Link to='/login'>Login</Link>
+        <Link to='/signup'>Signup</Link>
+      </nav>
     </div>
   );
 };
