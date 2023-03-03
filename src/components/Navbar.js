07d45a66e87ddbb906d6e47e4786e2297003a2ff
tahminaroshni <tahminatanni12@gmail.com>
2023-03-03 15:36:37 +0600

    logout functionality added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,33 @@
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
+  const { user, dispatch } = useAuthContext();
+
+  const handleLogout = () => {
+    // update UI
+    dispatch({ type: 'LOGOUT' });
+
+    // clear localstorage
+    localStorage.removeItem('user');
+  }
+
   return (
     <div className="h-20 bg-slate-900 text-slate-50 border-b border-sky-700 px-10 flex justify-between items-center">
       <h2>
         <Link to="/" className="text-sky-400 text-xl">Proxima</Link>
       </h2>
-      <nav className="flex gap-5">
-        <Link to='/login'>Login</Link>
-        <Link to='/signup'>Signup</Link>
+      <nav>
+        {
+          !user ? <div className="flex gap-5 items-center">
+            <Link to='/login'>Login</Link>
+            <Link to='/signup'>Signup</Link>
+          </div> : <div className="flex gap-5 items-center">
+            <p>{user?.email}</p>
+            <Link onClick={handleLogout} to='/logout' className="rounded bg-rose-500 text-rose-50 p-2 px-5">Logout</Link>
+          </div>
+        }
+
       </nav>
     </div>
   );
