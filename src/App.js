83fb23c9e2a33173072fd445b238d956668c320f
tahminaroshni <tahminatanni12@gmail.com>
2023-03-03 15:15:07 +0600

    route protected

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import { useAuthContext } from "./hooks/useAuthContext";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Projects from "./pages/Projects";
 import Signup from "./pages/Signup";
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <>
       <Navbar />
       <div className="app bg-slate-900 text-slate-50 min-h-screen container mx-auto py-5">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={user ? <Home /> : <Navigate to='/login' />} />
+          <Route path="/projects" element={user ? <Projects /> : <Navigate to='/login' />} />
+          <Route path="/login" element={!user ? <Login /> : <Navigate to='/' />} />
+          <Route path="/signup" element={!user ? <Signup /> : <Navigate to='/' />} />
+          <Route path="*" element={<Navigate to='/' />} />
         </Routes>
       </div>
       <Footer />
