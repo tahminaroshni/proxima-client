1d4c1fe9e689a9200f0b0d5f118fbae9574cb6bf
tahminaroshni <tahminatanni12@gmail.com>
2023-03-05 13:38:16 +0600

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <p
+        className='mx-auto p-10 bg-slate-900 text-sky-600 text-center text-sm rounded-b-lg border-t-sky-700 border-t'
+      >&copy; {new Date().getFullYear()} Proxima by <span className="hover:text-sky-200 duration-300">Tahmina Akter Tanni</span>. All rights reserved.</p>
+    </footer>
   );
 };
 
